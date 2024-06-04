64b3d9ece7e086d99617cc562b27b56e54ca8b14
Ibrahim <ibrahimazeem41@gmail.com>
2024-06-04 20:49:34 +0500

    work on github profile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
+import avatar from '../src/assets/avatar2.jpeg'
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="mx-auto bg-grey-400">
+      <div class="min-h-screen flex flex-col">
+        <NavBar />
+        <div class="flex flex-1">
+          <main class="bg-white-300 flex-1 p-3 overflow-hidden">
+            <div class="text-center flex justify-center">
+              <div class="p-4">
+                 <img class="inline-block w-52" style={{borderRadius: '100%'}} src={avatar} alt="" />
+              </div>
+              <div class="text-center border rounded p-4">
+                <p className=' text-xs'>
+Software Engineer 🧑‍💻 • Snapchat👻: ibrahimazeem41 • Gym Freak 🔩 • Love Challenges • We create our own demons Instagram: @ya.xheikh </p>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
