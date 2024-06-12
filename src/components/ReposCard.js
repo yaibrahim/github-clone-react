06f84cd461c5968e1888d773a46aae58e898b123
Ibrahim <ibrahimazeem41@gmail.com>
2024-06-12 19:42:47 +0500

    added favicon

--- src/components/ReposCard.js
+++ src/components/ReposCard.js
@@ -5,7 +5,7 @@ function ReposCard({name, link, children, darkMode}) {
     console.log(darkMode)
     return(
         <div className={`p-4 rounded-lg border text-left ${darkMode ? 'bg-[#111827] text-white' : 'bg-white text-black'}`}>
-            <h2 class="text-sm font-semibold mb-2">
+            <h2 className="text-sm font-semibold mb-2">
                 <FontAwesomeIcon icon={faBookBookmark} color="#626B76" /><FontAwesomeIcon /> <span className={`${darkMode ? 'bg-[#111827] text-white' : 'bg-white text-black'}`} ><a href={link} target="_blank" rel="noopener noreferrer">{name}</a></span>
             </h2>
             {children}
