fefb9b520b50739798df1713804e1ddfa37a867e
M Ibrahim <75537744+yaibrahim@users.noreply.github.com>
2025-01-29 01:46:42 +0500

    Update Experiences.js

--- src/main-components/Experiences.js
+++ src/main-components/Experiences.js
@@ -1,12 +1,13 @@
 import ExperienceTab from "../components/ExperienceTab";
-import {ProjectSouthville, ProjectDevntech, ProjectWorldSrc} from '../components/Projects'
+import {ProjectSouthville, ProjectDevntech, ProjectWorldSrc, ProjectBayt} from '../components/Projects'
 
 export default function Experiences({darkMode}) {
     return (
         <>
             <h1 className="text-2xl font-bold mb-4 text-left pt-3 text-black dark:text-white">Experience</h1>
             <div className="bg-white dark:bg-gray-800 p-4 rounded-lg border text-left">
-                <ExperienceTab designation="Software Engineer" company="Southville Solutions" date="July 2022 - Present" description={<ProjectSouthville />} darkMode={darkMode} />
+                <ExperienceTab designation="Software Engineer" company="Bayt.com" date="Aug 2024 - Present" description={<ProjectBayt />} darkMode={darkMode} />
+                <ExperienceTab designation="Software Engineer" company="Southville Solutions" date="July 2022 - July 2024" description={<ProjectSouthville />} darkMode={darkMode} />
                 <ExperienceTab designation="Associate Software Engineer" company="Devntech Pvt Ltd" date="Jan 2022 - Jun 2022" description={<ProjectDevntech />} darkMode={darkMode}/>
                 <ExperienceTab designation="Software Developer" company="WorldSrc (Freelance - Remote)" date="Nov 2020 - Nov 2021" description={<ProjectWorldSrc />} darkMode={darkMode} />
             </div>
